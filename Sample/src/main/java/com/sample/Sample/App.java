afc8038e82eada0229f6ed6583dd175d291456c2
gademahipal <mahipalreddy12@gmail.com>
2017-12-07 09:48:54 -0500

    First check-in to branch

--- Sample/src/main/java/com/sample/Sample/App.java
+++ Sample/src/main/java/com/sample/Sample/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("sample project!!");
+        System.out.println("sample project - branch1");
     }
 }
